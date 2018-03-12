2909af516abeaea352f590af17763e313fffc100
Leonardo Vásquez <leonardovavi@unitec.edu>
2018-03-11 20:56:02 -0600

    Finished

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,13 @@
 class ProductList extends React.Component {
-	constructor(props){
-		super(props);
-
-		this.state = {
-			products: [],
-		};
-
-		this.handleProductUpVote = this.handleProductUpVote.bind(this);
-	}
+	state = {
+		products: [],
+	};
 
 	componentDidMount(){
 		this.setState({products: Seed.products});
 	}
 
-	handleProductUpVote(productId) {
+	handleProductUpVote = (productId) => {
 		const nextProducts = this.state.products.map((product) => {
 			if(product.id === productId){
 				return Object.assign({}, product, {
@@ -61,7 +55,7 @@ class Product extends React.Component{
 		this.handleUpVote = this.handleUpVote.bind(this);
 	}
 
-	handleUpVote() {
+	handleUpVote = () => {
 		this.props.onVote(this.props.id);
 	}
 
