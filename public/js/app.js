32a934eb477e514ccab052cf80a4c2bf2f0b4d1f
Leonardo Vásquez <leonardovavi@unitec.edu>
2018-03-11 20:50:47 -0600

    Added UpVote Handling

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,70 @@
 class ProductList extends React.Component {
-  render() {
-   	const productComponents = Seed.products.map((product) => (
-		<Product
-			key={'Product-'+product.id}
-        	id={product.id}
-        	title={product.title}
-        	description={product.description}
-        	url={product.url}
-        	votes={product.votes}
-        	submittedAvatarUrl={product.submittedAvatarUrl}
-        	productImageUrl={product.productImageUrl}
-    	/>
-	));
+	constructor(props){
+		super(props);
 
-	return (
-		<div className='ui unstackable items'>
-			{productComponents}
-		</div>
-	);
-  }
+		this.state = {
+			products: [],
+		};
+
+		this.handleProductUpVote = this.handleProductUpVote.bind(this);
+	}
+
+	componentDidMount(){
+		this.setState({products: Seed.products});
+	}
+
+	handleProductUpVote(productId) {
+		const nextProducts = this.state.products.map((product) => {
+			if(product.id === productId){
+				return Object.assign({}, product, {
+					votes: product.votes + 1,
+				});
+			} else {
+				return product;
+			}
+		});
+		this.setState({
+			products: nextProducts,
+		});
+	}
+
+	render() {
+		const products = this.state.products.sort((a, b) => (
+	  		b.votes - a.votes
+	  	));
+	   	const productComponents = products.map((product) => (
+			<Product
+				key={'Product-'+product.id}
+	        	id={product.id}
+	        	title={product.title}
+	        	description={product.description}
+	        	url={product.url}
+	        	votes={product.votes}
+	        	submittedAvatarUrl={product.submittedAvatarUrl}
+	        	productImageUrl={product.productImageUrl}
+	        	onVote={this.handleProductUpVote}
+	    	/>
+		));
+
+		return (
+			<div className='ui unstackable items'>
+				{productComponents}
+			</div>
+		);
+	}
 }
 
 class Product extends React.Component{
+	constructor(props){
+		super(props);
+
+		this.handleUpVote = this.handleUpVote.bind(this);
+	}
+
+	handleUpVote() {
+		this.props.onVote(this.props.id);
+	}
+
 	render(){
 		return (
 			<div className='item'>
@@ -30,8 +73,8 @@ class Product extends React.Component{
 				</div>
 				<div className='middle aligned content'>
 					<div className='header'>
-						<a><i className='large caret up icon' /></a>
-						{this.props.title}
+						<a onClick={this.handleUpVote}><i className='large caret up icon' /></a>
+						{this.props.votes}
 					</div>
 					<div className='description'>
 						<a href={this.props.url}>
